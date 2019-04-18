48c17c6ce4fc4d40c222fba8b2401bba03f7aef1
Fan <snisiarc@gmail.com>
2019-04-18 02:38:18 -0400

    lower chase for sub_dir in url

--- scripts/ena_download.py
+++ scripts/ena_download.py
@@ -5,5 +5,5 @@ with open(sys.argv[1]) as f:
 	for lines in f:
 		line = lines.strip().split("\t")
 		ena_id = line[0].replace("000000", "")
-		sub_dir = ena_id[:2]
+		sub_dir = ena_id[:2].lower()
 		print "curl -O http://ftp.ebi.ac.uk/pub/databases/ena/wgs/public/" + sub_dir + "/" + ena_id +".fasta.gz"
